8915cfea4a35b4089b49da6df332a2285c61d2bf
gabriellunelli <gabriellunelli1203@gmail.com>
2026-09-01 09:02:21 -0300

    esqueleto do mini projeto já dado no material do curso.

---
+++ Desenvolvedor Back-End Node T4/modulo_1/semana5/aula2/carrinho-miniProjeto/carrinho.js
@@ -0,0 +1,11 @@
+function criarCarrinho() {
+    const itens = [];
+    // 1. total() somando preco * qtd
+    // 2. adicionar(nome, preco, qtd)
+    // 3. remover(nome)
+    // 4. resumo() com o texto final
+  }
+   
+  const aplicarCupom = (pct) => (valor) =>
+    // 5. devolva o valor com desconto
+  
