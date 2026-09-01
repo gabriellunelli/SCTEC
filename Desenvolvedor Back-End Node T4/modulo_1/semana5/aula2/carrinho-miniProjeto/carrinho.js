3a583740148e7649b6a2677fd31123538c743cf3
gabriellunelli <gabriellunelli1203@gmail.com>
2026-09-01 10:31:41 -0300

    versão ainda não funcional mas criada por mim

--- Desenvolvedor Back-End Node T4/modulo_1/semana5/aula2/carrinho-miniProjeto/carrinho.js
+++ Desenvolvedor Back-End Node T4/modulo_1/semana5/aula2/carrinho-miniProjeto/carrinho.js
@@ -1,11 +1,37 @@
 function criarCarrinho() {
     const itens = [];
-    // 1. total() somando preco * qtd
-    // 2. adicionar(nome, preco, qtd)
-    // 3. remover(nome)
-    // 4. resumo() com o texto final
+
+    return {
+        // 1. total() somando preco * qtd
+        total() {
+            return itens.preco * itens.qtd
+        },
+        // 2. adicionar(nome, preco, qtd)
+        adicionar(nome, preco, qtd) {
+            return itens.push({nome: nome, preco: preco, qtd: qtd})
+        },
+        // 3. remover(nome)
+        remover(nome) {
+            const itemARemover = itens.find(item => item === nome)
+            return itens.pop(itemARemover)
+        },
+        // 4. resumo() com o texto final
+        resumo() {
+            return console.log(`Valor total de ${this.total()}`)
+        }
+    }
+    
   }
+
+const carrinho = criarCarrinho()
+
+carrinho.adicionar("Geladeira", 2500, 1)
+carrinho.resumo()
+carrinho.adicionar("Celular", 1500, 2)
+carrinho.resumo()
+carrinho.remover("Geladeira")
+carrinho.resumo()
    
-  const aplicarCupom = (pct) => (valor) =>
+const aplicarCupom = (pct) => (valor) => {return valor * (pct / 100)}
     // 5. devolva o valor com desconto
   
